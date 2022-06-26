ad03b8f42cfe8e0270239463f47a7396a5c916ad
saber_guirat <s.guirat@digital-expert.io>
2022-06-26 16:38:40 +0200

    feat: setup wasm

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card class="logo py-4 d-flex justify-center">
+        <NuxtLogo />
+        <VuetifyLogo />
+      </v-card>
+      <v-card>
+        <v-card-title class="headline">
+          Welcome to the Vuetify + Nuxt.js template + wasm
+        </v-card-title>
+        <v-card-text> </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  mounted() {
+    // eslint-disable-next-line no-console
+    console.log(this.$wasm.add(1, 2))
+  },
+}
+</script>
---
+++ plugins/test.ts
@@ -0,0 +1,64 @@
+import '../static/wasm_exec'
+import Vue from 'vue'
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $wasm: {
+      add: WebAssembly.ExportValue
+      multiply: WebAssembly.ExportValue
+    }
+  }
+}
+
+const wasmBrowserInstantiate = async (
+  wasmModuleUrl: RequestInfo,
+  importObject: WebAssembly.Imports
+) => {
+  let response
+
+  // Check if the browser supports streaming instantiation
+  if (WebAssembly.instantiateStreaming) {
+    // Fetch the module, and instantiate it as it is downloading
+    response = await WebAssembly.instantiateStreaming(
+      fetch(wasmModuleUrl),
+      importObject
+    )
+  } else {
+    // Fallback to using fetch to download the entire module
+    // And then instantiate the module
+    const fetchAndInstantiateTask = async () => {
+      const wasmArrayBuffer = await fetch(wasmModuleUrl).then((response) =>
+        response.arrayBuffer()
+      )
+      return WebAssembly.instantiate(wasmArrayBuffer, importObject)
+    }
+
+    response = await fetchAndInstantiateTask()
+  }
+
+  return response
+}
+
+const go = new window.Go()
+
+const exports = async () => {
+  // Get the importObject from the go instance.
+  const importObject = go.importObject
+
+  // Instantiate our wasm module
+  const wasmModule = await wasmBrowserInstantiate('./main.wasm', importObject)
+  // Allow the wasm_exec go instance, bootstrap and execute our wasm module
+  go.run(wasmModule.instance)
+  const { add, multiply } = wasmModule.instance.exports
+  return { add, multiply }
+}
+
+const main = async () => {
+  const { add, multiply } = await exports()
+  Vue.prototype.$wasm = {
+    add,
+    multiply,
+  }
+}
+
+main()
